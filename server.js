6d711dffe40d84cc71911d41f446e8f97cff90ec
Anudeep <anudeepd@nbostech.com>
2019-08-27 21:22:03 +0530

    commit

---
+++ server.js
@@ -0,0 +1,120 @@
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+var request = require('request');
+
+const OktaJwtVerifier = require('@okta/jwt-verifier');
+var cors = require('cors');
+
+
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+    issuer: 'https://dev-432293.okta.com/oauth2/default',
+    clientId: '0oa14wenqdmlFynMp357',
+    // assertClaims: {
+    //     cid: '0oa13rvul8MFlTv6I357'
+    // },
+});
+
+/**
+ * A simple middleware that asserts valid access tokens and sends 401 responses
+ * if the token is not present or fails validation.  If the token is valid its
+ * contents are attached to req.jwt
+ */
+function authenticationRequired(req, res, next) {
+    const authHeader = req.headers.authorization || '';
+    const match = authHeader.match(/Bearer (.+)/);
+
+    if (!match) {
+        return res.status(401).end();
+    }
+
+    const accessToken = match[1];
+    const expectedAudience = '0oa13rvul8MFlTv6I357';
+    return request.post({
+        url: 'https://cpwnweqcs1.cepheid.pri:44316/sap/bc/sec/oauth2/authorize?response_type=code&client_id=TT-MPOWER&redirect_uri=http://localhost:3000/redirect&scope=ZQTC_EQUIPMENT_SRV_0001&state=anystate',
+        json: true
+    }, function (error, response, body) {
+        console.log(response);
+        next();
+    });
+    // return oktaJwtVerifier.verifyAccessToken(accessToken)
+    //     .then((jwt) => {
+    //         console.log(jwt)
+    //         req.jwt = jwt;
+    //         next();
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //         res.status(401).send("un authorized");
+    //     });
+}
+
+/**
+ * For local testing only!  Enables CORS for all domains
+ */
+app.use(cors());
+
+/**
+ * An example route that requires a valid access token for authentication, it
+ * will echo the contents of the access token if the middleware successfully
+ * validated the token.
+ */
+app.get('/secure', authenticationRequired, (req, res) => {
+    res.json(req.jwt);
+});
+
+
+
+/**
+ * Another example route that requires a valid access token for authentication, and
+ * print some messages for the user if they are authenticated
+ */
+app.get('/apiv1/messages', authenticationRequired, (req, res) => {
+    res.json([{
+        message: 'Hello, word!'
+    }]);
+}, (error) => {
+    console.log(error);
+});
+
+app.get('/redirect', (req, res) => {
+    res.json([{
+        message: 'Hello, word!'
+    }]);
+});
+
+app.get('/apiv2/messages', (req, res) => {
+    res.json([{
+        message: 'Hello, word!'
+    }]);
+});
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
+
+// use JWT auth to secure the api
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register', '/api/users/current'] }));
+
+// routes
+app.use('/login', require('./controllers/login.controller'));
+app.use('/register', require('./controllers/register.controller'));
+app.use('/app', require('./controllers/app.controller'));
+app.use('/api/users', require('./controllers/api/users.controller'));
+
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.redirect('/app');
+});
+
+// start server
+var server = app.listen(3000, function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+});
---
+++ services/user.service.js
@@ -0,0 +1,155 @@
+﻿var config = require('config.json');
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var Q = require('q');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('users');
+
+var service = {};
+
+service.authenticate = authenticate;
+service.getUser = getUser;
+service.create = create;
+service.update = update;
+service.delete = _delete;
+
+module.exports = service;
+
+function authenticate(username, password) {
+    var deferred = Q.defer();
+
+    db.users.findOne({ username: username }, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user && bcrypt.compareSync(password, user.hash)) {
+            // authentication successful
+            deferred.resolve(jwt.sign({ sub: user._id }, config.secret));
+        } else {
+            // authentication failed
+            deferred.resolve();
+        }
+    });
+
+    return deferred.promise;
+}
+
+function getUser(userParam) {
+    var deferred = Q.defer();
+    if (!userParam.aadhar) {
+        userParam.aadhar = "";
+    }
+    db.users.findOne(
+        { phone: userParam.phone, aadhar: userParam.aadhar },
+        function (err, user) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+            console.log(user);
+            if (user) {
+                // username already exists
+                deferred.resolve(user);
+            } else {
+                deferred.resolve();
+            }
+        });
+
+    return deferred.promise;
+}
+
+function create(userParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.users.findOne(
+        { phone: userParam.phone, aadhar: userParam.aadhar },
+        function (err, user) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+            console.log(user);
+            if (user) {
+                // username already exists
+                deferred.reject('Phone "' + userParam.phone + '" is already taken');
+            } else {
+                createUser();
+            }
+        });
+
+    function createUser() {
+        // set user object to userParam without the cleartext password
+
+        db.users.insert(
+            userParam,
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function update(_id, userParam) {
+    var deferred = Q.defer();
+
+    // validation
+    db.users.findById(_id, function (err, user) {
+        if (err) deferred.reject(err.name + ': ' + err.message);
+
+        if (user.username !== userParam.username) {
+            // username has changed so check if the new username is already taken
+            db.users.findOne(
+                { username: userParam.username },
+                function (err, user) {
+                    if (err) deferred.reject(err.name + ': ' + err.message);
+
+                    if (user) {
+                        // username already exists
+                        deferred.reject('Username "' + req.body.username + '" is already taken')
+                    } else {
+                        updateUser();
+                    }
+                });
+        } else {
+            updateUser();
+        }
+    });
+
+    function updateUser() {
+        // fields to update
+        var set = {
+            firstName: userParam.firstName,
+            lastName: userParam.lastName,
+            username: userParam.username,
+        };
+
+        // update password if it was entered
+        if (userParam.password) {
+            set.hash = bcrypt.hashSync(userParam.password, 10);
+        }
+
+        db.users.update(
+            { _id: mongo.helper.toObjectID(_id) },
+            { $set: set },
+            function (err, doc) {
+                if (err) deferred.reject(err.name + ': ' + err.message);
+
+                deferred.resolve();
+            });
+    }
+
+    return deferred.promise;
+}
+
+function _delete(_id) {
+    var deferred = Q.defer();
+
+    db.users.remove(
+        { _id: mongo.helper.toObjectID(_id) },
+        function (err) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+
+            deferred.resolve();
+        });
+
+    return deferred.promise;
+}
